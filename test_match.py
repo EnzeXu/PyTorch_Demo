38fdb664802ac0fc4f7da53542c3e1d66b0a4557
EnzeXu <xue20@wfu.edu>
2023-01-25 14:07:10 -0500

    updates

--- test_match.py
+++ test_match.py
@@ -18,7 +18,7 @@ class MatchModel(torch.nn.Module):
         self.setup_seed(0)
         self.config = config
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.params = torch.nn.Parameter(torch.tensor([0.49732122, 9.075426, 0.94164526, -8.735384, -5.5333996e-05, -2.2983756e-06, 0.0018869607, -9.3063536e-05])) # [0.00023, 1.85, 0.0, -0.001, 1.28]
+        self.params = torch.nn.Parameter(torch.tensor([0.41971564, 10.686993, 0.95063734, -8.744956, -3.3821703e-05, 0.00020107726, 0.00163968, -4.984618e-05])) # [0.00023, 1.85, 0.0, -0.001, 1.28]
         self.criterion = torch.nn.MSELoss().to(self.device)
         self.x_truth_numpy = np.asarray([3.0, 6.0, 9.0, 11.0, 12.0]) - 3.0
         self.x_truth = torch.tensor(self.x_truth_numpy, dtype=torch.float32).to(self.device)
@@ -91,9 +91,10 @@ class MatchModel(torch.nn.Module):
         x_plot = self.x_plot
         y_plot = self.f(x_plot, params_numpy)
         plt.figure(figsize=(8, 6))
-        plt.plot(x_plot, y_plot, c="b")
-        plt.scatter(x_truth, y_truth, c="r", label="truth")
-        plt.scatter(x_truth, y_predict, c="b", label="predict")
+        plt.plot(x_plot, y_plot, c="b", label="predict")
+        plt.scatter(x_truth, y_predict, facecolors="none", edgecolors="b", s=100, label="predict_points")
+        plt.scatter(x_truth, y_truth, c="r", s=100, marker="x", label="truth_points")
+
         plt.legend()
         plt.show()
         # plt.close()
@@ -105,3 +106,4 @@ if __name__ == "__main__":
     config = Config
     model = MatchModel(config)
     model.train_model()
+    # model.test_model()
